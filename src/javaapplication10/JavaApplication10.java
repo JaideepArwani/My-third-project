00f6fa3ec2c4db66189a92600b32294c1225a1ab
JaideepArwani <77190655+JaideepArwani@users.noreply.github.com>
2021-01-19 14:58:51 +0500

    Add files via upload

---
+++ src/javaapplication10/JavaApplication10.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author admin
+ */
+public class JavaApplication10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String name = "ali12";
+       String password = "ali12345";
+       String email = "ali1@gmail.com";
+       int age = 123;
+       
+       System.out.println(name);
+       System.out.println(age) ;
+       System.out.println(password);
+       System.out.println(email) ;
+       
+       
+       int num1=120;
+       int num2=110;
+       int res=num1/num2;
+       
+       System.out.println("this is plus "+res);
+       
+        int num3=120;
+        int num4=110;
+        int res1=num1/num2;
+        int res2=num1+num2;
+        int res3=num1*num2;
+        int res4=num1/num2;
+        int res5=num1*num2;
+        int res6=num1+num2;
+        int res7=num1%num2;
+
+        System.out.println("this is divide "+res);
+        System.out.println("this is plus "+res1);
+        System.out.println("this is multiply "+res2);
+        System.out.println("this is divide "+res3);
+        System.out.println("this is multiply "+res4);
+        System.out.println("this is plus "+res5);
+        System.out.println("this is reminder "+res6);
+        
+               
+               
+               
+    }
+    
+}
